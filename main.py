af32b50972be9f4b83a26f1448e8162599a4847b
Mads Jenkins <madelinehjenkins@gmail.com>
2022-04-25 17:15:44 +0100

    switching to loading full data from plane only

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from hrtfdata.torch.planar import CHEDARPlane, ARIPlane
+from hrtfdata.torch.full import ARI
 from hrtfdata.torch import collate_dict_dataset
 from torch.utils.data import DataLoader
 from pathlib import Path
 
 
-def load_data(data_folder, load_function, plane, domain, side):
+def load_data(data_folder, load_function):
     base_dir = Path('/Users/madsjenkins/Imperial/HRTF/Volumes/home/HRTF Datasets')
-    return load_function(base_dir / data_folder, plane, domain=domain, side=side)
+    return load_function(base_dir / data_folder)
+
+
+def main():
+    ds = load_data(data_folder='ARI', load_function=ARI)
+    # DataLoader(ds, collate_fn=collate_dict_dataset)
+    print(len(ds))
 
 
 if __name__ == '__main__':
-    ds = load_data(data_folder='ARI', load_function=ARIPlane,
-                   plane='median', domain='magnitude_db', side='left')
-    DataLoader(ds, collate_fn=collate_dict_dataset)
-    print(len(ds))
+    main()
