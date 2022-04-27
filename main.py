64f3a0efbabfac779a8c3d533402603178319046
Mads Jenkins <madelinehjenkins@gmail.com>
2022-04-27 16:12:41 +0100

    full data import now working

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from torch.utils.data import DataLoader
 from pathlib import Path
 
 
-def load_data(data_folder, load_function):
+def load_data(data_folder, load_function, domain, side):
     base_dir = Path('/Users/madsjenkins/Imperial/HRTF/Volumes/home/HRTF Datasets')
-    return load_function(base_dir / data_folder)
+    return load_function(base_dir / data_folder,
+                         feature_spec={"hrirs": {'side': side, 'domain': domain}},
+                         target_spec={"side": {}},
+                         group_spec={"subject": {}})
 
 
 def main():
-    ds = load_data(data_folder='ARI', load_function=ARI)
+    ds = load_data(data_folder='ARI', load_function=ARI, domain='magnitude_db', side='left')
     # DataLoader(ds, collate_fn=collate_dict_dataset)
     print(len(ds))
 
