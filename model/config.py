d1032550afd337884d8d8d3c9940f00eb57517b5
Mads Jenkins <madelinehjenkins@gmail.com>
2022-06-15 11:46:35 +0100

    update config file

--- model/config.py
+++ model/config.py
@@ -1,5 +1,7 @@
 import json
 
+import torch
+
 
 class Config:
     """Config class
@@ -7,19 +9,26 @@ class Config:
     def __init__(self, tag):
         self.tag = tag
         self.path = f'runs/{self.tag}'
+        # data dirs
         self.data_path = 'data/Example_ppp.png'
+        self.train_hrtf_dir = 'projected_data/train'
+        self.valid_hrtf_dir = 'projected_data/valid'
+        self.hrtf_size = 16
+
+        self.upscale_factor = 4
         self.net_type = 'gan'
         self.l = 64
         self.n_phases = 2
         # Training hyperparams
         self.batch_size = 64
+        self.num_workers = 4
         # betas for Adam optimizer
         self.beta1 = 0.9
         self.beta2 = 0.999
         self.num_epochs = 5 # was originally 250
         self.iters = 1000
-        self.lrg = 0.0001
-        self.lr = 0.0001
+        self.lr_gen = 0.0001
+        self.lr_dis = 0.0001
         self.Lambda = 10
         self.critic_iters = 10
         self.lz = 4
@@ -46,11 +55,8 @@ class Config:
             j = json.load(f)
             for k, v in j.items():
                 setattr(self, k, v)
-
-    def get_net_params(self):
-        return self.dk, self.ds, self.df, self.dp, self.gk, self.gs, self.gf, self.gp
     
     def get_train_params(self):
-        return self.l, self.batch_size, self.beta1, self.beta2, self.num_epochs, self.iters, self.lrg, self.lr, self.Lambda, self.critic_iters, self.lz, self.nz
+        return self.l, self.batch_size, self.beta1, self.beta2, self.num_epochs, self.iters, self.lr_gen, self.lr_dis, self.Lambda, self.critic_iters, self.lz, self.nz
 
 
