47ad73351bddbf14a258e65d4d00fcf261b3ef8f
Mads Jenkins <madelinehjenkins@gmail.com>
2022-06-15 11:03:14 +0100

    add CUDAPrefetcher from SRGAN-Pytorch

---
+++ model/dataset.py
@@ -0,0 +1,43 @@
+import torch
+
+
+class CUDAPrefetcher:
+    """Use the CUDA side to accelerate data reading.
+    Args:
+        dataloader (DataLoader): Data loader. Combines a dataset and a sampler, and provides an iterable over the given dataset.
+        device (torch.device): Specify running device.
+    """
+
+    def __init__(self, dataloader, device: torch.device):
+        self.batch_data = None
+        self.original_dataloader = dataloader
+        self.device = device
+
+        self.data = iter(dataloader)
+        self.stream = torch.cuda.Stream()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.batch_data = next(self.data)
+        except StopIteration:
+            self.batch_data = None
+            return None
+
+        with torch.cuda.stream(self.stream):
+            for k, v in self.batch_data.items():
+                if torch.is_tensor(v):
+                    self.batch_data[k] = self.batch_data[k].to(self.device, non_blocking=True)
+
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        batch_data = self.batch_data
+        self.preload()
+        return batch_data
+
+    def reset(self):
+        self.data = iter(self.original_dataloader)
+        self.preload()
+
+    def __len__(self) -> int:
+        return len(self.original_dataloader)
