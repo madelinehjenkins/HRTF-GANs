731f3b5b0d87a4ad423d67d01f43b82dd7af0e33
Mads Jenkins <madelinehjenkins@gmail.com>
2022-08-04 15:30:41 +0100

    increase epochs

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class Config:
         # Training hyperparams
         self.batch_size = 32
         self.num_workers = 4
-        self.num_epochs = 20  # was originally 250
+        self.num_epochs = 250  # was originally 250
         self.lr_gen = 0.0001
         self.lr_dis = 0.0001
         # how often to train the generator
