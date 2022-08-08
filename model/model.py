c7b086dafb917ec01ef0b1e3f08aecc39b5df3d0
Mads Jenkins <madelinehjenkins@gmail.com>
2022-08-08 13:02:02 +0100

    update dimensions in discriminator comments

--- model/model.py
+++ model/model.py
@@ -55,20 +55,21 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.features = nn.Sequential(
-            # input size. (128) x 20 x 20
+            # input size. (128) x 5 x 16 x 16
             CubeSpherePadding2D(1),
             CubeSphereConv2D(128, 64, (3, 3), (1, 1), bias=True),
             nn.LeakyReLU(0.2, True),
-            # state size. (64) x 18 x 18
+            # state size. (64) x 5 x 16 x 16
             CubeSpherePadding2D(1),
             CubeSphereConv2D(64, 64, (3, 3), (1, 1), bias=False),
             nn.BatchNorm3d(64),
             nn.LeakyReLU(0.2, True),
+            # state size. (64) x 5 x 16 x 16
             CubeSpherePadding2D(1),
             CubeSphereConv2D(64, 128, (3, 3), (1, 1), bias=False),
             nn.BatchNorm3d(128),
             nn.LeakyReLU(0.2, True),
-            # state size. (128) x 16 x 16
+            # state size. (128) x 5 x 16 x 16
             CubeSpherePadding2D(1),
             CubeSphereConv2D(128, 128, (3, 3), (2, 2), bias=False),
             nn.BatchNorm3d(128),
@@ -77,7 +78,7 @@ class Discriminator(nn.Module):
             CubeSphereConv2D(128, 256, (3, 3), (1, 1), bias=False),
             nn.BatchNorm3d(256),
             nn.LeakyReLU(0.2, True),
-            # state size. (256) x 8 x 8
+            # state size. (256) x 5 x 8 x 8
             CubeSpherePadding2D(1),
             CubeSphereConv2D(256, 256, (3, 3), (2, 2), bias=False),
             nn.BatchNorm3d(256),
@@ -86,7 +87,7 @@ class Discriminator(nn.Module):
             CubeSphereConv2D(256, 512, (3, 3), (1, 1), bias=False),
             nn.BatchNorm3d(512),
             nn.LeakyReLU(0.2, True),
-            # state size. (512) x 4 x 4
+            # state size. (512) x 5 x 4 x 4
             CubeSpherePadding2D(1),
             CubeSphereConv2D(512, 512, (3, 3), (2, 2), bias=False),
             nn.BatchNorm3d(512),
